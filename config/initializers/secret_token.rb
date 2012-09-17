fa7f9bbc036a44c0181ab49a034805c2f99b6c5e
Marius Lucian Pop <marius.lucian.pop@gmail.com>
2012-09-17 18:54:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trackit::Application.config.secret_token = 'f7ae864171c4553cb0896599d8b0dee60c786088a03b2d6952a3318795de0fd6b64716687316a36b32ae737b4a029d76479078ce759d629b17be082e088d17eb'
---
+++ spec/requests/add_activities_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe "AddActivities" do
+  it "adds an activity if add activity button is clicked" do
+    visit new_activity_path
+  end
+end
