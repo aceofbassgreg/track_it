95399082d4e3eceb7eaa59beca10d247893e5bea
Marius Lucian Pop <marius.lucian.pop@gmail.com>
2012-09-24 21:48:33 -0400

    fix template users/edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   before_filter :authenticate_user!, except: [:sign_in, :register]
-  
+    
   helper_method :graph
 end
