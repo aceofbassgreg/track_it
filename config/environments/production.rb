23a18e607ac06dac6512603ad2f6128cd2055dc6
Marius Lucian Pop <marius.lucian.pop@gmail.com>
2012-09-27 15:36:56 -0400

    added devise-confirmable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,14 @@ Trackit::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'mlpinit.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587,
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_PASSWORD"]
+  }
 
   # Enable threaded mode
   # config.threadsafe!
