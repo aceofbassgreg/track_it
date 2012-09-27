add83d48e4d2cf51006823da1be0f2af6c0388f2
Marius Lucian Pop <marius.lucian.pop@gmail.com>
2012-09-27 16:05:12 -0400

    changed mandrill for gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,10 +19,13 @@ Trackit::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address   => "smtp.mandrillapp.com",
-    :port      => 587,
-    :user_name => ENV["MANDRILL_USERNAME"],
-    :password  => ENV["MANDRILL_PASSWORD"]
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "railscasts.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
   }
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   
