35555f85d3a4ba7e7e0667926f89994af898f483
Marius Lucian Pop <marius.lucian.pop@gmail.com>
2012-10-01 19:33:43 -0400

    Reonfigured devise for development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,18 @@ Trackit::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "mlpinit.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+  config.action_mailer.default_url_options = { :host => 'trackit.mlpinit.com' }
 
   # Enable threaded mode
   # config.threadsafe!
