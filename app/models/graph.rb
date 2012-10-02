0d58ca9e8d5579288b0336c99a6c6f89af917b87
Marius Lucian Pop <marius.lucian.pop@gmail.com>
2012-10-01 22:53:41 -0400

    TDD Graph.date returns time_trackers between 2 times

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -1,6 +1,12 @@
 class Graph
 
-  def initialize(activity, start_date, end_date)
-
+  def initialize(activity, start_time, end_time)
+    @activity = activity
+    @start_time = start_time.beginning_of_day
+    @end_time = end_time.end_of_day
+  end
+  
+  def data
+    @activity.time_trackers.where(clock_in: @start_time..@end_time)
   end
 end
--- spec/models/graph_spec.rb
+++ spec/models/graph_spec.rb
@@ -1,18 +1,34 @@
 require 'spec_helper'
 
 describe Graph do
-  it "raises an error without activity, start and end date" do
+  it "raises an error without activity, start and end time" do
     expect{ Graph.new}.to raise_error(ArgumentError)
   end
   
-  it "initializez with an activity, a start and an end date" do
+  it "initializez with an activity, a start and an end time" do
     expect{ Grap.new(activity, start_date, end_date)}.to_not raise_error(ArgumentError)
   end
   
-  it "extracts time trackers between start date and end date" do 
-    activity = create(:activity)
-    create(:time_tracker,activity_id: activity.id)
-    create(:time_tracker,activity_id: activity.id, clock_in: Time.now)
-    pending
+  before :all do
+    @activity = create(:activity)
+  end
+  
+  it "extracts time trackers between start time and end time" do 
+    time_tracker_present = create(:time_tracker,activity_id: @activity.id)
+    present = time_tracker_present.clock_in
+    graph = Graph.new(@activity, present, present)
+    graph.data.size.should eql(1)
+  end
+  
+  it "does NOT extract time trackers before start time" do 
+    time_tracker_past = create(:time_tracker,activity_id: @activity.id, clock_in: Time.now - 10.days, clock_out: Time.now - 10.days)
+    graph = Graph.new(@activity, Time.now, Time.now)
+    graph.data.size.should eql(0)
+  end
+  
+  it "does NOT extract time trackers after end time" do 
+    time_tracker_future = create(:time_tracker,activity_id: @activity.id, clock_in: Time.now + 10.days, clock_out: Time.now + 10.days)
+    graph = Graph.new(@activity, Time.now, Time.now)
+    graph.data.size.should eql(0)
   end
 end
