5c3b64c0ee7b71d8443f9f866850357b61e194f5
Marius Lucian Pop <marius.lucian.pop@gmail.com>
2012-10-02 21:36:55 -0400

    Finished refactorying the Graph model

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -6,19 +6,22 @@ class Graph
     @end_time = end_time.end_of_day
   end
   
+  def all_data
+    (empty_data + mapped_data).compact.sort_by {|hash| hash[:date]}
+  end
+  
   def data
     data = @activity.time_trackers.where(clock_in: @start_time..@end_time).order("clock_in ASC")
     data.group_by {|hash| hash[:clock_in].to_date}
   end
   
   def mapped_data
-    new_data = {}
     array = []
     data.each do |key,value| 
+      new_data = {}
       new_data[:date] = key
       new_data[:minutes] = time(value)
       array << new_data
-      new_data = {}
     end
     array
   end
@@ -27,5 +30,24 @@ class Graph
     total_time = 0
       array.each { |hash| total_time += hash[:clock_out] - hash[:clock_in] }
     total_time.to_i
+  end
+  
+  def empty_data
+    (@start_time.to_date..@end_time.to_date).to_a.map do |n|
+      if not exists?(n)
+        hash = {}
+        hash[:date] = n
+        hash[:minutes] = 0
+        hash
+      end
+    end
+  end
+  
+  def exists?(value)
+    boolean = false
+    mapped_data.each do |hash|
+      boolean = true if hash[:date] == value
+    end
+    boolean
   end
 end
