7e3ef1d3069cad3c30528fd89900311d96141a5b
Marius Lucian Pop <marius.lucian.pop@gmail.com>
2012-10-06 12:14:07 -0400

    Created total_time feature.

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -37,8 +37,8 @@ class Graph
     return true if params_nil?
     return true if @days
     # start_time and end_time become nil if they are blank
-    @start_time = @start_time.to_time.beginning_of_day
-    @end_time = @end_time.to_time.end_of_day
+    @start_time.to_time
+    @end_time.to_time
     return true if @start_time and @end_time
     rescue ArgumentError
   end
@@ -70,13 +70,40 @@ class Graph
   def day
     @start_time.to_date.strftime("%d")
   end
+ 
+# We create a hash with all values of zero.
   
+  def prepare
+    new_hash = {}
+    (@start_time.to_date..@end_time.to_date).to_a.each { |value| new_hash["#{value}"] = 0}
+    new_hash
+  end
+
+# We enter the actual data in a new hash called data_hash
+# We need to make sure that the key date value has the same format as 
+# the other keys otherwise an extra element will pop up (strftime)...
+
+  def data_hash
+    prepared_data = prepare
+    add = mapped_data.each {|key,value| prepared_data[key.strftime('%Y-%m-%d')] = value}
+    prepared_data
+  end
+  
+# We extract the values from data_hash and place them in an array for 
+# highcharts  
   def minutes
     minutes = []
     data_hash.each {|key,value| minutes << value}
     minutes
-  end
+  end  
 
+  def total_time
+    sum = 0
+    minutes.collect  {|min| sum += min}.last
+    hours = sum / 60
+    minutes = sum % 60 
+    "#{hours}h #{minutes}m"
+  end
 # We collect the data ordered by clock_in in ascending order. We group 
 # the data by the date of clock in. And we make sure that if the last
 # element is not clocked out it is not included in the graph.  
@@ -87,19 +114,22 @@ class Graph
     data.group_by {|hash| hash[:clock_in].to_date}
   end
 
-# We use the time method to calculate the time a user spend on an activity
+# We use the time method to calculate the time a user spends on an activity
 # in a day and we map that as our new value  
  
   def mapped_data
     data = collect_data
-    data.map {|key,value| data[key] = time(value)}
+    data.map {|key,value| data[key] = time_per_day(value)}
     data
   end
   
-  def time(array)
+# Returns the amount of minutes spent on an activity in the given array
+# Usually a day
+  
+  def time_per_day(array)
     total_time = 0
       array.each { |hash| total_time += (hash[:clock_out] - hash[:clock_in]) / 60 }
-    total_time.to_i
+    total_time.ceil
   end
  
   def prepare
@@ -114,8 +144,6 @@ class Graph
   def data_hash
     prepared_data = prepare
     add = mapped_data.each {|key,value| prepared_data[key.strftime('%Y-%m-%d')] = value}
-    p "---------"
-    p prepared_data.inspect
     prepared_data
   end
 end
