93beac6ef44d8d0076467152530bf5e8f8e88452
Marius Lucian Pop <marius.lucian.pop@gmail.com>
2012-10-05 20:17:46 -0400

    precompile not working on heroku. attempting fix 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,8 +16,7 @@ Trackit::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-  config.assets.precompile += ['highcharts.js']
-
+  
   # Generate digests for assets URLs
   config.assets.digest = true
 
